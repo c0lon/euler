ef61c0ef0bd2db2b96d0be95de11b2d9940e94e2
Collin Barth <cpbarth92@gmail.com>
2015-11-25 23:08:04 -0500

    made second perms argument optional

--- utils.py
+++ utils.py
@@ -72,32 +72,19 @@ def permutations(start, end, obj=None):
                 curr.append(i)
 
                 if len(curr) == end - start + 1:
-<<<<<<< HEAD
                     yield curr
-                else:
-                    for perm in _permutations(curr):
-                        yield perm
-=======
-                    yield curr[:]
                 else:
                     for _perm in _permutations(curr):
                         yield _perm
->>>>>>> 5be01c8ed56bb71dde115a126c092248af2b738d
 
                 curr.remove(i)
 
-    for perm in _permutations([]):
-        yield perm
-
-<<<<<<< HEAD
-=======
     perms = _permutations([])
 
     if obj:
         pass
 
     return perms
->>>>>>> 5be01c8ed56bb71dde115a126c092248af2b738d
 
 # generate a new file
 def new_file(args):
@@ -180,21 +167,15 @@ if __name__ == '__main__':
 
     elif op == 'perms':
         start = int(sys.argv[2])
-        end   = int(sys.argv[3])
+        try:
+            end = int(sys.argv[3])
+        except IndexError:
+            end = start
+            start = 0
 
-<<<<<<< HEAD
         count = 0
         for p in permutations(start, end):
             print(p)
             count += 1
 
-        print('\nthere are {} permutations for {} <= x <= {}'.format(count, start, end))
-=======
-        ps = permutations(start, end)
-        count = 0
-        for p in ps:
-            print(p)
-            count += 1
-
         print('\nthere are {} permutations for {} <= x <= {}'.format(count, start, end))    
->>>>>>> 5be01c8ed56bb71dde115a126c092248af2b738d
